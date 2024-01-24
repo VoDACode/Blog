c451c374d1dec1409d3f12ecc62e7562e27f9e46
VoDACode <dimavovk201112@gmail.com>
2024-01-24 14:30:32 +0200

    Partial loading of posts is implemented

--- blog.client/src/app/components/home-page/home-page.component.ts
+++ blog.client/src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { PostModelResponse } from 'src/app/models/post.model';
 import { UserResponseModel } from 'src/app/models/user.model';
 import { PostApiService } from 'src/app/services/post-api.service';
@@ -10,15 +10,25 @@ import { UserApiService } from 'src/app/services/user-api.service';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+
+  private page: number = 1;
+  private pageSize: number = 10;
+  private maxPageCount: number = 50;
+  private isLoading: boolean = false;
+
+  public loading: boolean = true;
   public posts: PostModelResponse[] = [];
+  public userModel: UserResponseModel = new UserResponseModel();
   public get hasPosts() {
     return this.posts.length > 0;
   }
-  public userModel: UserResponseModel = new UserResponseModel();
+
   constructor(private postApi: PostApiService, private userApi: UserApiService) { }
+
   ngOnInit(): void {
-    this.postApi.getPosts(1, 10).subscribe(res => {
+    this.postApi.getPosts(this.page, this.pageSize).subscribe(res => {
       this.posts = res.data?.items || [];
+      this.loading = false;
     });
     this.userApi.getMe().subscribe(res => {
       this.userModel = res.data || new UserResponseModel();
@@ -30,5 +40,28 @@ export class HomePageComponent implements OnInit {
     if (index >= 0) {
       this.posts.splice(index, 1);
     }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    const pos = window.scrollY;
+    const max = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    if (pos == max && this.page < this.maxPageCount) {
+      this.loadMore();
+    }
+  }
+
+  private loadMore() {
+    if(this.loading) return;
+    this.page++;
+    this.loading = true;
+    this.postApi.getPosts(this.page, this.pageSize).subscribe(res => {
+      if (res.data != null && res.data.items.length > 0) {
+        this.maxPageCount = res.data.totalPagesCount;
+        this.posts.push(...res.data?.items || []);
+        this.isLoading = false;
+        this.loading = false;
+      }
+    });
   }
 }
